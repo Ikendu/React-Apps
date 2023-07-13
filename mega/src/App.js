9da698c395c99297289a2bd4df3574ef1894f59d
Aniede Chibundu David <ndubest56@gmail.com>
2023-07-13 20:08:35 +0100

    set name for initial testing

--- mega/src/App.js
+++ mega/src/App.js
@@ -5,16 +5,20 @@ import { useGlobalContext } from "./contex";
 function App() {
   const [name, setName] = useGlobalContext();
 
-  setName("Chibundu");
-  
-
   console.log(name);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>More to come</h1>
-        <button onClick={()=>{setName("Chibundu")}}
+        <button
+          onClick={() => {
+            setName("Chibundu");
+          }}
+        >
+          Change Name
+        </button>
+        <h3>{name}</h3>
       </header>
     </div>
   );

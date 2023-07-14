0bd17d84848aa1d1805c7a07cf1ee7a5b64ea9d7
Aniede Chibundu David <ndubest56@gmail.com>
2023-07-14 10:48:38 +0100

    data entry re-render performance solved

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
 //import ProjectState from "./projects/02-useEffect/02-fetch-data";
-import ProjectState from "./projects/11-performance/01-lower-state";
+import ProjectState from "./projects/11-performance/02-lower-state";
 function App() {
   return (
     <div className="container">
---
+++ advanced-react/src/projects/11-performance/02-lower-state/index.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { data } from "../../../data";
+import List from "./List";
+
+const Form = ({ addPerson }) => {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      alert("Please Provide Name Value");
+      return;
+    }
+    addPerson(name);
+    setName("");
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-input"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <button className="btn btn-block" type="submit">
+        submit
+      </button>
+    </form>
+  );
+};
+
+const LowerStateChallenge = ({ handleSubmit }) => {
+  const [people, setPeople] = useState(data);
+
+  const addPerson = (name) => {
+    const fakeId = Date.now();
+    const newPerson = { id: fakeId, name };
+    setPeople([...people, newPerson]);
+  };
+  return (
+    <section>
+      <Form addPerson={addPerson} />
+      <List people={people} />
+    </section>
+  );
+};
+export default LowerStateChallenge;

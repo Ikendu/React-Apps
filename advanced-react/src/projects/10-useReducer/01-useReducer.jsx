853d848de8b88508950f4f730478116fd7a7b6e6
Aniede Chibundu David <ndubest56@gmail.com>
2023-07-14 05:04:48 +0100

    using useReducer to rewrite methods and functions

--- advanced-react/src/projects/10-useReducer/01-useReducer.jsx
+++ advanced-react/src/projects/10-useReducer/01-useReducer.jsx
@@ -4,8 +4,10 @@ import { data, people } from "../../data";
 const defaultState = {
   people: data,
 };
-
-const reducer = () => {};
+const reducer = (state, action) => {
+  if (action.type === "RESTORE") return { ...state, people: data };
+  else if (action.type === "CLEAR_LIST") return { ...state, people: [] };
+};
 
 const ReducerBasics = () => {
   //const [people, setPeople] = useState(data);
@@ -13,6 +15,11 @@ const ReducerBasics = () => {
 
   const restoreAll = () => {
     //setPeople(data);
+    dispatch({ type: "RESTORE" });
+  };
+  const handleClick = () => {
+    //setPeople([]);
+    dispatch({ type: "CLEAR_LIST" });
   };
 
   // const removeItem = (id) => {
@@ -21,7 +28,7 @@ const ReducerBasics = () => {
   // };
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
@@ -34,11 +41,11 @@ const ReducerBasics = () => {
       })}
       {
         //people === data ?
-        people.length > 0 ? (
+        state.people.length > 0 ? (
           <button
             className="btn"
             style={{ marginTop: "2rem" }}
-            onClick={() => setPeople([])}
+            onClick={handleClick}
           >
             clear items
           </button>

775b9dff82bdb05bf50389b865f01efc8f2aca50
Aniede Chibundu David <ndubest56@gmail.com>
2023-07-15 23:26:07 +0100

    using lazy and suspense to load page on user demand

--- advanced-react/src/projects/11-performance/04-react-18/index.jsx
+++ advanced-react/src/projects/11-performance/04-react-18/index.jsx
@@ -1,15 +1,18 @@
-import { useState, useTransition } from "react";
+import { useState, useTransition, lazy, Suspense } from "react";
+const SlowComponent = lazy(() => import("./SlowComponent"));
+
 const LatestReact = () => {
   const [text, setText] = useState("");
   const [items, setItems] = useState([]);
   const [isLoading, setTransition] = useTransition();
+  const [show, setShow] = useState(false);
 
   const handleChange = (e) => {
     setText(e.target.value);
 
     // slow down CPU
     {
-      /* keep the transition in the background untill the loading is complete */
+      /* keep the transition in the background until the loading is complete */
     }
     setTransition(() => {
       const newItems = Array.from({ length: 5000 }, (_, index) => {
@@ -33,6 +36,15 @@ const LatestReact = () => {
         />
       </form>
       <h4>Items Below</h4>
+
+      <button
+        className="btn"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        toggle
+      </button>
       {/*display Loading while waiting for data to finish load */}
       {isLoading ? (
         "Loading..."
@@ -46,6 +58,12 @@ const LatestReact = () => {
         >
           {items}
         </div>
+      )}
+
+      {show && (
+        <Suspense fallback={"Loading..."}>
+          <SlowComponent />
+        </Suspense>
       )}
     </section>
   );
